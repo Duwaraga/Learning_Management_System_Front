b297020dfaf9444cdb04c957ec325a39dc2c661d
Duwaraga <18duwa@gmail.com>
2022-06-12 16:50:41 -0700

    Added form to get the student details

--- src/Student.js
+++ src/Student.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-const Student = () => {
+function Student(props) {
+  const [name, setName] = useState("");
+  const [registrationNumber, setRegistrationNumber] = useState("");
+  var [student, setStudent] = useState([]);
+
   return (
-    <div>Student</div>
-  )
+    <>
+      <form onSubmit={handleSubmit}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <TextField
+            style={{ flex: 0.5 }}
+            label="Name"
+            name="name"
+            id="outlined-start-adornment"
+            className=""
+            variant="outlined"
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <TextField
+            style={{ flex: 0.5 }}
+            label="Registration Number"
+            id="outlined-start-adornment"
+            className=""
+            name="regNo"
+            onChange={(e) => setRegistrationNumber(e.target.value)}
+            variant="outlined"
+          />
+
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </div>
+        </div>
+      </form>
+
+    </>
+  );
 }
 
-export default Student
+export default Student;
